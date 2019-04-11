fead6f9cd7e870573162af86e50e7489d5cee0bc
daehuikim <40377750+daehuikim@users.noreply.github.com>
2019-04-11 18:26:51 +0900

    Add files via upload

---
+++ assn/assn3/README.txt
@@ -0,0 +1,20 @@
+Assignment #3
+
+This assignment is recommended to be done in a group of two people.
+It is worth of 4% of the total final score.
+
+The assignment is to develop an Internet text+voice chatting program using sockets.
+Basically, you are to add a voice-to-voice communication function to your assignment #2.
+You should be able to chat voice and text simultaneously (i.e., should be able to exchange text messages while voice chatting).
+
+You are to submit the following:
+
+1. Design and Implementation Document (5 or less pages)  =>assn3.docx
+2. Source code (well-documented) =>보고서에 소스코드 해석 첨부해두었습니다.
+3. Executable codes (include libraries for stand-alone running) =>audioclient.py,audioserver.py,textclient.py,textserver.py
+4. Screen capture video of the program execution =>실행장면.mp4
+
+Have fun!
+
+실행순서
+audioserver실행->textserver실행->audioclient,textcleint접속
---
+++ assn/assn3/audioclient.py
@@ -0,0 +1,45 @@
+import socket
+import pyaudio
+import _thread
+
+# record
+CHUNK = 1024 # 512
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 20000
+
+host=input('Enter the address:')
+port=33300
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+p = pyaudio.PyAudio()
+
+receive_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+send_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+print("Voice chat running")
+
+def receive_data():
+    while True:
+        try:
+            data = s.recv(1024)
+            receive_stream.write(data)
+        except:
+            pass
+
+
+def send_data():
+    while True:
+        try:
+            data = send_stream.read(CHUNK)
+            s.sendall(data)
+        except:
+            pass
+
+
+_thread.start_new_thread(receive_data, ())
+_thread.start_new_thread(send_data, ())
+
+while True:
+    pass
